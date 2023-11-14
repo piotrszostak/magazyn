77fa0a3eae150d75dcfec9337cd04cebbe739561
Piotr Szostak <pio.szo@proton.me>
2023-11-14 17:29:00 +0100

    Restructure exit func into menu, add items list

--- magazyn.py
+++ magazyn.py
@@ -1,6 +1,28 @@
-def exit_script():
-    ask_user = input("What would you like to do?")
-    while ask_user != "exit":
-        ask_user = input("What would you like to do?")
+def menu():
+    while True:
+        user_input = input("What would you like to do?\n")
+        
+        if user_input == '':
+            #func1()
+            pass
+        elif user_input == 'exit':
+            exit()
+        else:
+            continue
 
-exit_script()
+menu()
+
+items = [
+    {name: kurtka,
+     quantity: 3,
+     unit: pcs,
+     unit_price: 300},
+    {name: bluza,
+     quantity: 5,
+     unit: pcs,
+     unit_price: 200},
+    {name: koszulka,
+     quantity: 13,
+     unit: pcs,
+     unit_price: 50}
+]
