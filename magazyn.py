f5aec522edf590ae9e1ad6ece4837cac0109ca07
Piotr Szostak <pio.szo@proton.me>
2023-11-14 18:14:30 +0100

    Add display table function

--- magazyn.py
+++ magazyn.py
@@ -1,10 +1,36 @@
+
+items = [
+    {'name': 'kurtka',
+     'quantity': 3,
+     'unit': 'pcs',
+     'unit_price': 300},
+    {'name': 'bluza',
+     'quantity': 5,
+     'unit': 'pcs',
+     'unit_price': 200},
+    {'name': 'koszulka',
+     'quantity': 13,
+     'unit': 'pcs',
+     'unit_price': 50}
+]
+
+def list_items():
+    print(items)
+    
+def get_items():
+    print("Name\tQuantity\tUnit\tUnit Price (PLN)")
+    for item in items:
+        #TODO
+        print(item['name'] +'\t'+ str(item['quantity']) +'\t'+item['unit'] +'\t'+ str(item['unit_price']))
+
 def menu():
     while True:
-        user_input = input("What would you like to do?\n")
+        user_input = input("What would you like to do? (show, list, exit)\n")
         
-        if user_input == '':
-            #func1()
-            pass
+        if user_input == 'list':
+            list_items()
+        elif user_input == 'show':
+            get_items()
         elif user_input == 'exit':
             exit()
         else:
@@ -12,17 +38,3 @@ def menu():
 
 menu()
 
-items = [
-    {name: kurtka,
-     quantity: 3,
-     unit: pcs,
-     unit_price: 300},
-    {name: bluza,
-     quantity: 5,
-     unit: pcs,
-     unit_price: 200},
-    {name: koszulka,
-     quantity: 13,
-     unit: pcs,
-     unit_price: 50}
-]
